b0e2346b7decaa97124e5ddc2b2ba78c25c12a80
Howard M Chen <31971182+hchen88@users.noreply.github.com>
2018-10-11 01:33:35 -0700

    Add files via upload

---
+++ Triangle2.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Triangle2.h
+ * Author: howar
+ *
+ * Created on October 10, 2018, 9:54 PM
+ */
+
+#ifndef TRIANGLE2_H
+#define TRIANGLE2_H
+
+class Triangle2 {
+public:
+    Triangle2();
+    Triangle2(const Triangle2& orig);
+    virtual ~Triangle2();
+private:
+
+};
+
+#endif /* TRIANGLE2_H */
+
